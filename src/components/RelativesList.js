65758a6d5018f504cc58ec1358322f0e16ea316b
Amritanand25 <amritanand.jnv25@gmail.com>
2021-06-06 20:58:57 +0530

    second commit
    second commit

---
+++ src/components/RelativesList.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function RelativesList() {
+    return (
+        <>
+         <ol>
+             <li key="relativeListItem1">Father</li>
+             <li key="relativeListItem2">Mother</li>
+             <li key="relativeListItem3">Sister</li>
+             <li key="relativeListItem4">Brother</li>
+         </ol>
+        </>
+    )
+}
+
+export default RelativesList;
